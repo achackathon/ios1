9a1f17508198bee714023568b4293f94c43766b6
Jota Melo <jpmfagundes@gmail.com>
2016-08-20 10:42:33 -0300

    Created product cell

---
+++ Troca Legal/ProductCell.h
@@ -0,0 +1,13 @@
+//
+//  ProductCell.h
+//  Troca Legal
+//
+//  Created by Jota Melo on 20/08/16.
+//  Copyright © 2016 Avenue Code Brazil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProductCell : UICollectionViewCell
+
+@end
---
+++ Troca Legal/ProductCell.m
@@ -0,0 +1,22 @@
+//
+//  ProductCell.m
+//  Troca Legal
+//
+//  Created by Jota Melo on 20/08/16.
+//  Copyright © 2016 Avenue Code Brazil. All rights reserved.
+//
+
+#import "ProductCell.h"
+
+@interface ProductCell ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *priceLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *typeImageView;
+
+@end
+
+@implementation ProductCell
+
+@end
